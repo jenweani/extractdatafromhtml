bf7eb7484edef708d17de74d2592b82534ed0663
john <jenweani@gmail.com>
2024-02-23 14:40:47 +0100

    initial commit

---
+++ extract.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func extractData(node *html.Node, class string, result *[]string) {
+	if node.Type == html.ElementNode && node.Data == "span" {
+		for _, attr := range node.Attr {
+			if attr.Key == "class" && strings.Contains(attr.Val, class) {
+				// Found the target span tag
+				var text string
+				// Extract text content from child nodes
+				for child := node.FirstChild; child != nil; child = child.NextSibling {
+					if child.Type == html.ElementNode && child.Data == "span" {
+						text += child.FirstChild.Data
+					}
+				}
+				*result = append(*result, text)
+			}
+		}
+	}
+
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		extractData(child, class, result)
+	}
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run extract_data.go input.html outExtract.txt")
+		os.Exit(1)
+	}
+
+	inputFileName := os.Args[1]
+	outputFileName := os.Args[2]
+	className := os.Args[3]
+
+	file, err := os.Open(inputFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	doc, err := html.Parse(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var extractedData []string
+	extractData(doc, className, &extractedData)
+
+	// Save extracted data to the output file
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outputFile.Close()
+
+	for _, data := range extractedData {
+		_, err := outputFile.WriteString(data + "\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("Extraction completed. Data saved to", outputFileName)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module extractFromHtml
+
+go 1.21.0
+
+require golang.org/x/net v0.21.0
